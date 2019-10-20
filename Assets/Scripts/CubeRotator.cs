5340f0ebccb624ed9cfc3b4e698ee5c26465def2
Sergey Krasnogorov <krasnogorovsergey@gmail.com>
2019-10-20 22:11:35 +0300

    Added main menu. Added art for main menu.

---
+++ Assets/Scripts/CubeRotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    /// <summary>
+    /// Class for rotate cube in main menu
+    /// </summary>
+    public class CubeRotator : MonoBehaviour
+    {
+        /// speed of rotation
+        [SerializeField]
+        private float _rotationSpeed = 100.0f;
+        /// direction of rotation
+        [SerializeField]
+        private Vector3 _rotationDirection = Vector3.one;
+
+        private void FixedUpdate()
+        {
+            gameObject.transform.Rotate(_rotationDirection, Time.deltaTime * _rotationSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    /// <summary>
+    /// Class controls events in main mmenu
+    /// </summary>
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _cube = null;
+        [SerializeField]
+        private Button _settingsButton = null;
+        [SerializeField]
+        private Button _achievementButton = null;
+        [SerializeField]
+        private Button _aboutButton = null;
+
+        private void OnEnable()
+        {
+            _settingsButton.onClick.AddListener(OnSettingsButtonClicked);
+            _achievementButton.onClick.AddListener(OnAchievementButtonClicked);
+            _aboutButton.onClick.AddListener(OnAboutButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _settingsButton.onClick.RemoveAllListeners();
+            _achievementButton.onClick.RemoveAllListeners();
+            _aboutButton.onClick.RemoveAllListeners();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    if (hit.collider.gameObject == _cube)
+                    {
+                        Debug.LogError("Click on cube");
+                    }
+                }
+            }
+        }
+        private void OnSettingsButtonClicked()
+        {
+            Debug.LogError("Setiings button clicked");
+        }
+
+        private void OnAchievementButtonClicked()
+        {
+            Debug.LogError("Achievement button clicked");
+        }
+
+        private void OnAboutButtonClicked()
+        {
+            Debug.LogError("About button clicked");
+        }
+    }
+}
