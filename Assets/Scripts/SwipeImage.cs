f1b29ea3fd8175f8636be427cbe16ac0d3ebf306
Sergey Krasnogorov <krasnogorovsergey@gmail.com>
2019-10-25 12:00:48 +0300

    Added hide and show animations

--- Assets/Scripts/SwipeImage.cs
+++ Assets/Scripts/SwipeImage.cs
@@ -19,13 +19,23 @@ namespace UI
         /// textl label
         [SerializeField]
         private Text _text = null;
+        /// animation component
+        [SerializeField]
+        private Animation _animation = null;
         /// callback for swipe action
         private Action<bool> _resultCallback;
         /// start position
         private Vector3 _position = Vector3.zero;
         /// max distance for success swipe effect
-        private const float MAX_DISTANCE = 100.0f;
+        private const float MAX_DISTANCE = 300.0f;
+        /// speed of hiding element
+        private const float SPEED = 10000.0f;
+        /// offset for hiding
+        private const float HIDE_DISTANCE = 5000.0f;
 
+        private bool _isExit = false;
+        private Vector3 _exitPosition;
+       
         private void Start()
         {
             _position = transform.position;
@@ -51,8 +61,36 @@ namespace UI
         {
             float distance = Vector3.Distance(_position, eventData.position);
             
-            transform.position = _position;
-            _resultCallback?.Invoke(distance > MAX_DISTANCE);
+            if (distance > MAX_DISTANCE)
+            {
+                _isExit = true;
+                _exitPosition = eventData.position;
+                _exitPosition.x += (transform.position.x > _position.x) ? HIDE_DISTANCE : -HIDE_DISTANCE;
+                
+            }
+            else
+            {
+                transform.position = _position;
+                _animation.Play();
+                _resultCallback?.Invoke(false);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (_isExit)
+            {
+                float step = SPEED * Time.deltaTime; 
+                transform.position = Vector3.MoveTowards(transform.position, _exitPosition, step);
+
+                if (Vector3.Distance(transform.position, _exitPosition) < Mathf.Epsilon)
+                {
+                    _isExit = false;
+                    transform.position = _position;
+                    _animation.Play();
+                    _resultCallback?.Invoke(true);
+                }
+            }
         }
     }
 }
