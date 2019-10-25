097d3fc9c4b6c99bdcf881690d44f7f2bf799547
Sergey Krasnogorov <krasnogorovsergey@gmail.com>
2019-10-25 14:25:01 +0300

    Fixed using list of indexes

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -2,6 +2,7 @@
 using Data;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -25,9 +26,14 @@ namespace UI
 
         /// index of selected image
         private int _selectedIndex = 0;
+        /// list with unused indexes of picture
+        public List<int> _unusedIndexes = new List<int>();
+
         /// key of selected index
         private const string SELECTED_INDEX_KEY = "selected_key";
-
+        /// count of shuffles
+        private const int SHUFFLE_COUNT = 3;
+        
         private void Start()
         {
             _selectedIndex = SaveManager.Instance.GetIntForKey(SELECTED_INDEX_KEY, 0);
@@ -53,25 +59,46 @@ namespace UI
         /// <summary>
         /// Display current image
         /// </summary>
-        private void ShowImage()
+        private void ShowImage(bool isRandom = false)
         {
-            if (_selectedIndex >= _imageCollection.Length)
-            {
-                _selectedIndex = 0;
-            }
-            _swipeImage.DisplayImage(_imageCollection.GetItem(_selectedIndex), (result) =>
+            int index = isRandom ? GetNextIndex() : _selectedIndex;
+            SaveManager.Instance.SetIntForKey(SELECTED_INDEX_KEY, index);
+            _swipeImage.DisplayImage(_imageCollection.GetItem(index), (result) =>
             {
                 if (result)
-                {
-                    _selectedIndex++;
-                    if (_selectedIndex >= _imageCollection.Length)
-                    {
-                        _selectedIndex = 0;
-                    }
-                    SaveManager.Instance.SetIntForKey(SELECTED_INDEX_KEY, _selectedIndex);
-                    ShowImage();
+                {                    
+                    ShowImage(true);
                 }
             });
+        }
+
+        private int GetNextIndex()
+        {
+            int ret = 0;
+            if (_unusedIndexes.Count == 0)
+            {
+                GenerateIndexesList();                
+            }
+            ret = _unusedIndexes[Random.Range(0, _unusedIndexes.Count - 1)];
+            _unusedIndexes.Remove(ret);
+            return ret;
+        }
+
+        private void GenerateIndexesList()
+        {
+            _unusedIndexes = Enumerable.Range(0, _imageCollection.Length - 1).ToList();
+            // shuffle this list
+            int count = _unusedIndexes.Count;
+            for (int i = 0; i < SHUFFLE_COUNT; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    int randIndex = Random.Range(0, count);
+                    int tmp = _unusedIndexes[j];
+                    _unusedIndexes[j] = _unusedIndexes[randIndex];
+                    _unusedIndexes[randIndex] = tmp;
+                }
+            }
         }
     }
 }
